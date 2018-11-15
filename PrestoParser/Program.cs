8717e57d5ebc9271846f5ead8c65a7101729751f
quinnbot2 <quinnbot@gmail.com>
2018-11-15 13:47:12 -0800

    Initial project structure

---
+++ PrestoParser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductFileParser;
+
+namespace PrestoParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser test = new Parser();
+
+            test.Parse("dummy filename");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProductFileParser/Parser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductFileParser
+{
+    public class Parser
+    {
+        string filename;
+
+        public void Parse(string inFilename)
+        {
+            filename = inFilename;
+
+            Console.WriteLine("File Parsed");
+        }
+    }
+}
